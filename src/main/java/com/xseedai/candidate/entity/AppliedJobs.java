a83fc6273366501894b00d9084d680b88f611a2b
Prajwal Patil <Prajwal Patil@TCOGNITION>
2024-03-21 07:34:38 -0400

    entities created

---
+++ src/main/java/com/xseedai/candidate/entity/AppliedJobs.java
@@ -0,0 +1,37 @@
+package com.xseedai.candidate.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "AppliedJobs")
+public class AppliedJobs {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long AppliedJobId;
+	
+	private Long appliedJobId;
+	
+	@ManyToOne()
+	@JoinColumn(name = "candidateId")
+	@JsonIgnore
+	private Candidate candidate;
+
+}
---
+++ src/main/java/com/xseedai/candidate/repository/SaveJobRepository.java
@@ -0,0 +1,9 @@
+package com.xseedai.candidate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xseedai.candidate.entity.SaveJob;
+
+public interface SaveJobRepository extends JpaRepository<SaveJob, Long>{
+
+}
