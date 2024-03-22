5c051a7bc58709106d9e1b07099af0c61fecfb7e
Roshan Farkate <Roshan Farkate@TCOGNITION>
2024-03-22 12:03:31 +0530

    Swagger and cadidate details changes

--- src/main/java/com/xseedai/candidate/CandidateApplication.java
+++ src/main/java/com/xseedai/candidate/CandidateApplication.java
@@ -3,6 +3,23 @@ package com.xseedai.candidate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(info = @Info(title = "CANDIDATE MICRO-SERVICE REST APIs", description = "Candidate Service REST APIs Documentation", version = "v1.0", contact = @Contact(name = "Prajwal,Atharva,Roshan,Khushbu,Soham,Sonal,Pranjali", email = "	xseedai@tcognition.com"
+
+), license = @License(name = "tCognition", url = "https://tcognition.com/"
+
+)
+
+), externalDocs = @ExternalDocumentation(description = "MicroServices Overview and Architecture", url = "https://docs.google.com/document/d/1tYMCRNRIOJeK01Ynx88bq6Iw7iVLECXG/edit?usp=drive_web&ouid=118309295115974221063&rtpof=true"
+
+)
+
+)
 @SpringBootApplication
 public class CandidateApplication {
 
